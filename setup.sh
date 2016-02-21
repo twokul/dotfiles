cfe00dab75085d6d4d51406289323c0372f4b807
Alex Navasardyan <alex.navasardyan@gmail.com>
2016-02-20 21:17:12 -0800

    npmrc

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+ln -s ~/.dotfiles/.npmrc ~/.npmrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.gitingore_global ~/.gitingore_global
